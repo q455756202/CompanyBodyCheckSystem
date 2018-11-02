acd428f13bc04d6d607c4f89b74f56a6ecf2f7f2
zhw <510666592@qq.com>
2018-11-02 14:25:09 +0800

    重新开始登陆

--- ccbs-controller/src/main/java/team/yingyingmonster/ccbs/controller/GatewayAction.java
+++ ccbs-controller/src/main/java/team/yingyingmonster/ccbs/controller/GatewayAction.java
@@ -1,10 +1,23 @@
 package team.yingyingmonster.ccbs.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author Juer Whang <br/>
  * - project: CompanyBodyCheckSystem
  * - create: 11:50 2018/11/2
  * -
  **/
+
+@Controller
+@RequestMapping("/gateway")
 public class GatewayAction {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "login/index";
+    }
+
+
 }
