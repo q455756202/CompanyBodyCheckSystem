dcc1d6641ebe1c0c3994b8e0a2b57fc444740e25
zhw <510666592@qq.com>
2018-11-02 15:50:13 +0800

    重新开始进行登陆流程

--- ccbs-controller/src/main/java/team/yingyingmonster/ccbs/controller/GatewayAction.java
+++ ccbs-controller/src/main/java/team/yingyingmonster/ccbs/controller/GatewayAction.java
@@ -2,11 +2,16 @@ package team.yingyingmonster.ccbs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import team.yingyingmonster.ccbs.common.ImageUtil;
 import team.yingyingmonster.ccbs.database.bean.Account;
-import team.yingyingmonster.ccbs.database.mapper.AccountMapper;
 import team.yingyingmonster.ccbs.service.serviceinterface.AccountService;
+
+
+import javax.servlet.http.HttpServletRequest;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
 
 /**
  * @author Juer Whang <br/>
@@ -24,15 +29,30 @@ public class GatewayAction {
 
     @RequestMapping("/index")
     public String index(){
-        return "login/index";
+        return "gateway/index";
     }
 
     @RequestMapping("/login")
-    public String loginAction(Long accountId,String accountPassword){
+    public String login(Long accountId,String accountPassword){
         Account account= new Account();
 
         account=accountService.login(accountId,accountPassword);
         return "common/index.html";
     }
 
+    @RequestMapping("/code-image")
+    public ResultMessage codeImage(HttpServletRequest req){
+        Map<String,BufferedImage> imageb=ImageUtil.createImage();
+        //得到验证码
+        String code=imageb.keySet().iterator().next();
+        req.getSession().setAttribute("keyCode",code);
+        //得到图片
+        BufferedImage image=imageb.get(code);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+
+
+
+    }
 }
