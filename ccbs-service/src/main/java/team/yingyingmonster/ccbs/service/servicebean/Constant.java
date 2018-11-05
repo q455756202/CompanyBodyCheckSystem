db146caf74b59d1fa31966bd5d2d75c09055511c
Juer Genie <JuerGenie@qq.com>
2018-11-05 10:23:51 +0800

    更新了一部分Bean类。

---
+++ ccbs-service/src/main/java/team/yingyingmonster/ccbs/service/servicebean/Constant.java
@@ -0,0 +1,35 @@
+package team.yingyingmonster.ccbs.service.servicebean;
+
+/**
+ * @author Juer Whang <br/>
+ * - project: CompanyBodyCheckSystem
+ * - create: 9:12 2018/11/5
+ * -
+ **/
+public class Constant {
+    public static final Integer
+            /**
+             * 角色 - 团检单位
+             */
+            ROLE_COMPANY    = 1,
+            /**
+             * 角色 - 医生
+             */
+            ROLE_DOCTOR     = 2,
+            /**
+             * 角色 - 总检医生
+             */
+            ROLE_GENERALDOC = 3,
+            /**
+             * 角色 - 前台服务
+             */
+            ROLE_RECEPTION  = 4,
+            /**
+             * 角色 - 管理员
+             */
+            ROLE_ADMIN      = 5,
+            /**
+             * 角色 - 超级管理员
+             */
+            ROLE_SUPERADMIN = 6;
+}
