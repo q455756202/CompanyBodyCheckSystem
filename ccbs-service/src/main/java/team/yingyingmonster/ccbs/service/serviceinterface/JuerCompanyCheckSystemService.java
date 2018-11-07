d62c2973dbd89e9563146422c5330f75979f0574
Juer Genie <JuerGenie@qq.com>
2018-11-07 17:00:58 +0800

    添加了一些通用工具类。完成了团检单报名到数据库插入的流程。

--- ccbs-service/src/main/java/team/yingyingmonster/ccbs/service/serviceinterface/JuerCompanyCheckSystemService.java
+++ ccbs-service/src/main/java/team/yingyingmonster/ccbs/service/serviceinterface/JuerCompanyCheckSystemService.java
@@ -3,6 +3,6 @@ package team.yingyingmonster.ccbs.service.serviceinterface;
 import team.yingyingmonster.ccbs.database.bean.juergenie.JuerCompanyCheckEntity;
 
 public interface JuerCompanyCheckSystemService {
-    JuerCompanyCheckEntity getCompanyCheckEntity(Long companyEntity);
+    JuerCompanyCheckEntity getCompanyCheckEntity(Long accountid);
     boolean registerCompanyCheck(JuerCompanyCheckEntity juerCompanyCheckEntity) throws Exception;
 }
