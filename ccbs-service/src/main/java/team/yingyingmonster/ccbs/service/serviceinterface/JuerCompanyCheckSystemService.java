8e3911ab03d4aaa51cfcfc0a48537d3bc17b0219
Juer Genie <JuerGenie@qq.com>
2018-11-08 21:50:26 +0800

    none.

--- ccbs-service/src/main/java/team/yingyingmonster/ccbs/service/serviceinterface/JuerCompanyCheckSystemService.java
+++ ccbs-service/src/main/java/team/yingyingmonster/ccbs/service/serviceinterface/JuerCompanyCheckSystemService.java
@@ -3,6 +3,7 @@ package team.yingyingmonster.ccbs.service.serviceinterface;
 import team.yingyingmonster.ccbs.database.bean.Check;
 import team.yingyingmonster.ccbs.database.bean.juergenie.JuerCombo;
 import team.yingyingmonster.ccbs.database.bean.juergenie.JuerCompanyCheckEntity;
+import team.yingyingmonster.ccbs.database.bean.juergenie.JuerUser;
 
 import java.util.List;
 
@@ -10,4 +11,5 @@ public interface JuerCompanyCheckSystemService {
     JuerCompanyCheckEntity getCompanyCheckEntity(Long accountid);
     boolean registerCompanyCheck(JuerCompanyCheckEntity juerCompanyCheckEntity) throws Exception;
     JuerCombo addCustomCombo(List<Check> checkList) throws Exception;
+    List<JuerUser> getJuerUser(Long companyid);
 }
