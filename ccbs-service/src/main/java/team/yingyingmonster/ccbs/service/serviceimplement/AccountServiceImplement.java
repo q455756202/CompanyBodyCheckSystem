0b77e970a814f6fcb9b3430a336a6c586a39d29a
zhw <510666592@qq.com>
2018-11-08 16:26:41 +0800

    解析excel文件，体检人名单提交数据库

--- ccbs-service/src/main/java/team/yingyingmonster/ccbs/service/serviceimplement/AccountServiceImplement.java
+++ ccbs-service/src/main/java/team/yingyingmonster/ccbs/service/serviceimplement/AccountServiceImplement.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import team.yingyingmonster.ccbs.database.bean.Account;
 import team.yingyingmonster.ccbs.database.bean.Company;
+import team.yingyingmonster.ccbs.database.bean.User;
+import team.yingyingmonster.ccbs.database.mapper.juergenie.JuerCompanyMapper;
 import team.yingyingmonster.ccbs.database.mapper.zhw.ZhaoAccountMapper;
 import team.yingyingmonster.ccbs.service.serviceinterface.AccountService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,6 +22,9 @@ import java.util.List;
 public class AccountServiceImplement implements AccountService {
     @Autowired
     private ZhaoAccountMapper zhaoAccountMapper;
+
+    @Autowired
+    private JuerCompanyMapper juerCompanyMapper;
 
     @Override
     public Account login(Long accountId, String accountPassword) {
@@ -63,6 +69,40 @@ public class AccountServiceImplement implements AccountService {
         Company company=null;
         company=zhaoAccountMapper.selectCompany(accountId);
         return company;
+    }
+
+    /**
+     * 通过账户ID查找公司ID
+     *
+     * */
+    @Override
+    public Long findCompanyId(Long accountId) {
+        Long companyId=juerCompanyMapper.selectCompanyByAccountId(accountId).getCompanyid();
+        return companyId;
+    }
+
+    /**
+     * 插入体检人员信息
+     *
+     * */
+    @Override
+    public Integer insertUsers(Long companyId, List<String[]> userList) {
+        Long userId=zhaoAccountMapper.getNewUserId();
+        User user=null;
+        List<User> users=new ArrayList<User>();
+        for(String[] userInfo: userList){
+                user.setUserid(userId);
+                user.setCompanyid(companyId);
+                user.setUsername(userInfo[0]);
+                user.setUsergender(Short.valueOf(userInfo[1]));
+                user.setUserage(Short.valueOf(userInfo[2]));
+                user.setUserphone(userInfo[3]);
+                user.setUsercardcode(userInfo[4]);
+                users.add(user);
+        }
+        Integer result=zhaoAccountMapper.insertUsers(users);
+        System.out.println("插入信息的条数："+result);
+        return result;
     }
 
 }
