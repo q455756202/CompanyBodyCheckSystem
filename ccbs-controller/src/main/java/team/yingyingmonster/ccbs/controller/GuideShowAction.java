a70a594d9ef9d373c5c867dc18782b53d9fc1ca1
Juer Genie <JuerGenie@qq.com>
2018-11-09 11:21:51 +0800

    Merge branch 'devolop' of https://github.com/JuerGenie/CompanyBodyCheckSystem into devolop
    # Conflicts:
    #	ccbs-database/src/main/java/team/yingyingmonster/ccbs/database/mapper/dingli/DingCompanyMapper.java
    #	ccbs-service/src/main/java/team/yingyingmonster/ccbs/service/serviceinterface/AccountService.java

--- ccbs-controller/src/main/java/team/yingyingmonster/ccbs/controller/GuideShowAction.java
+++ ccbs-controller/src/main/java/team/yingyingmonster/ccbs/controller/GuideShowAction.java
@@ -1,11 +1,40 @@
 package team.yingyingmonster.ccbs.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import team.yingyingmonster.ccbs.bean.ResultMessage;
+import team.yingyingmonster.ccbs.database.bean.Account;
+import team.yingyingmonster.ccbs.database.mapper.juergenie.JuerCompanyMapper;
+import team.yingyingmonster.ccbs.service.servicebean.Constant;
+import team.yingyingmonster.ccbs.service.serviceinterface.GuideShowService;
+
+import javax.servlet.http.HttpSession;
+
 /**
  * @author Juer Whang <br/>
  * - project: CompanyBodyCheckSystem
  * - create: 9:29 2018/11/9
  * - 导检单展示模块
  **/
+@Controller
+@RequestMapping("/guide-show")
 public class GuideShowAction {
+    @Autowired
+    private GuideShowService guideShowService;
+    @Autowired
+    private JuerCompanyMapper juerCompanyMapper;
 
+    @RequestMapping("/index")
+    public String index() {
+        return "guide-show/success";
+    }
+
+    @RequestMapping("/get-guide")
+    @ResponseBody
+    public ResultMessage getGuide(HttpSession session) {
+        Long accountid = ((Account) session.getAttribute(Constant.SESSION_LOGIN_ACCOUNT)).getAccountid();
+        return ResultMessage.createSuccessMessage("success!", guideShowService.getTeamformList(juerCompanyMapper.selectCompanyByAccountId(accountid).getCompanyid()));
+    }
 }
--- ccbs-service/src/main/java/team/yingyingmonster/ccbs/service/serviceimplement/GuideShowServiceImplement.java
+++ ccbs-service/src/main/java/team/yingyingmonster/ccbs/service/serviceimplement/GuideShowServiceImplement.java
@@ -1,5 +1,12 @@
 package team.yingyingmonster.ccbs.service.serviceimplement;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import team.yingyingmonster.ccbs.database.bean.Teamform;
+import team.yingyingmonster.ccbs.database.mapper.juergenie.JuerTeamformMapper;
+import team.yingyingmonster.ccbs.service.serviceinterface.GuideShowService;
+
+import java.util.List;
+
 /**
  * @author Juer Whang <br/>
  * - project: CompanyBodyCheckSystem
@@ -7,4 +14,11 @@ package team.yingyingmonster.ccbs.service.serviceimplement;
  * -
  **/
 public class GuideShowServiceImplement implements GuideShowService {
+    @Autowired
+    private JuerTeamformMapper juerTeamformMapper;
+
+    @Override
+    public List<Teamform> getTeamformList(Long companyid) {
+        return juerTeamformMapper.selectTeamformByCompanyId(companyid);
+    }
 }
