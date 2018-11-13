c7f306b4b395f71357f78e963b76da092dc7b6b3
Juer Genie <JuerGenie@qq.com>
2018-11-13 10:12:15 +0800

    修改渲染页面。

--- ccbs-database/src/main/java/team/yingyingmonster/ccbs/database/bean/juergenie/JuerUser.java
+++ ccbs-database/src/main/java/team/yingyingmonster/ccbs/database/bean/juergenie/JuerUser.java
@@ -1,5 +1,6 @@
 package team.yingyingmonster.ccbs.database.bean.juergenie;
 
+import lombok.*;
 import team.yingyingmonster.ccbs.database.bean.Check;
 import team.yingyingmonster.ccbs.database.bean.User;
 
@@ -11,6 +12,9 @@ import java.util.List;
  * - create: 16:11 2018/11/8
  * -
  **/
+@Getter
+@Setter
+@ToString
 public class JuerUser extends User {
     Long usercheckid;
     List<Check> checkList;
