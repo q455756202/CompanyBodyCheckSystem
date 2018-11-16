b29c40af691cd90aaf864fb40cf7af1ffe48ea62
陈志辉 <82651323@qq.com>
2018-11-16 14:54:37 +0800

    1

--- ccbs-controller/src/main/java/team/yingyingmonster/ccbs/controller/AllReportAction.java
+++ ccbs-controller/src/main/java/team/yingyingmonster/ccbs/controller/AllReportAction.java
@@ -1,7 +1,7 @@
 package team.yingyingmonster.ccbs.controller;
 
 import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
+import com.lowagie.text.DocumentException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,10 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import team.yingyingmonster.ccbs.bean.ResultMessage;
 import team.yingyingmonster.ccbs.common.CommonUtil;
+
 import team.yingyingmonster.ccbs.czh.PdfUitl;
+import team.yingyingmonster.ccbs.template.PdfUtil;
 import team.yingyingmonster.ccbs.database.bean.Report;
 import team.yingyingmonster.ccbs.database.mapper.czh.AllReportMapper;
 
+import java.io.*;
 import java.util.List;
 
 /**
@@ -23,7 +26,8 @@ import java.util.List;
 public class AllReportAction {
     @Autowired
     private AllReportMapper allReportMapper;
-    private PdfUitl pdfUitl =new PdfUitl();
+    private PdfUtil pdfUtil =new PdfUtil();
+    private PdfUitl pdfUitl=new PdfUitl();
     private CommonUtil commonUtil =new CommonUtil();
     /**
      * 跳转界面
@@ -43,7 +47,6 @@ public class AllReportAction {
         PageHelper.startPage(1,6);
         PageHelper.orderBy("reportid");
         List<Report> allreport=allReportMapper.selectAllReport();
-
         return ResultMessage.createSuccessMessage("获取的团检报告",allreport);
     }
     /**
@@ -51,13 +54,18 @@ public class AllReportAction {
      */
     @RequestMapping("/report-summary")
     @ResponseBody
-    private ResultMessage getReportSummary(@RequestBody Long reportid){
-        String pdfpath=pdfUitl.PdfPath(reportid);
-        Report report;
-        report=allReportMapper.selectReportSummary(reportid);
-        String pdfString=new String(CommonUtil.fromBase64(report.getReportsummary()));
-        pdfUitl.htmlCodeComeFromFile(pdfString,pdfpath);
-        return ResultMessage.createSuccessMessage("获取pdf文件的路径",pdfpath);
+    private ResultMessage getReportSummary(@RequestBody Long reportid) throws IOException, DocumentException {
+        String pdfpath = pdfUitl.PdfPath(reportid);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(pdfpath));
+            Report report;
+            report = allReportMapper.selectReportSummary(reportid);
+            String pdfString = new String(CommonUtil.fromBase64(report.getReportsummary()));
+            pdfUtil.createPdfFromHtmlFile(pdfString, fileOutputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return ResultMessage.createSuccessMessage("获取pdf文件的路径", pdfpath);
     }
 }
 
